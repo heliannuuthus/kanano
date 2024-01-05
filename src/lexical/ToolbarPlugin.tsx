f538723177ddcf2bb66c1f81a0bc246e9675096b
heliannuuthus <heliannuuthus@gmail.com>
2024-01-06 00:56:15 +0800

    component

--- src/lexical/ToolbarPlugin.tsx
+++ src/lexical/ToolbarPlugin.tsx
@@ -1,13 +1,15 @@
 import { styled } from "@mui/system";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { $findMatchingParent } from "@lexical/utils";
+import { $findMatchingParent, mergeRegister } from "@lexical/utils";
 import {
   $getSelection,
   $isRangeSelection,
   $isRootOrShadowRoot,
+  $setSelection,
   CLICK_COMMAND,
   COMMAND_PRIORITY_CRITICAL,
   ElementNode,
+  LexicalEditor,
   RangeSelection,
   SELECTION_CHANGE_COMMAND,
 } from "lexical";
@@ -23,58 +25,77 @@ export default function ToolbarPlugin({
   const [editor] = useLexicalComposerContext();
   const [activeEditor, setActiveEditor] = useState(editor);
   const [selectStart, setSelectStart] = useState(false);
-  const [selectEnd, setSelectEnd] = useState(false);
 
-  const $updateToolbar = useCallback(() => {}, [activeEditor]);
+  const handleMouseup = useCallback(() => {
+    editor.update(() => {
+      const selection = $getSelection();
+      console.log(selectStart);
 
-  useEffect(() => {
-    return editor.registerCommand(
-      SELECTION_CHANGE_COMMAND,
-      (_payload, newEditor) => {
-        setSelectStart(true);
-        setActiveEditor(newEditor);
-        return false;
-      },
-      COMMAND_PRIORITY_CRITICAL
-    );
-  }, [editor, $updateToolbar]);
-
-  useEffect(() => {
-    return editor.registerCommand(
-      CLICK_COMMAND,
-      (_payload, newEditor) => {
-        setSelectEnd(true);
-        const selection = $getSelection();
-        console.log(selection);
-        if ($isRangeSelection(selection)) {
-          if (
-            (selection as RangeSelection).anchor.offset ==
-            (selection as RangeSelection).focus.offset
-          ) {
-            setSelectEnd(false);
-            setSelectStart(false);
-            setIsShowToolbar(null);
-          } else {
-            if (selectStart && selectEnd) {
-              const anchorNode: ElementNode = (
-                selection as RangeSelection
-              ).anchor.getNode();
-              const elementDOM = activeEditor.getElementByKey(
-                anchorNode.getKey()
-              );
-              console.log("end");
-              setIsShowToolbar(elementDOM);
-            }
-          }
+      if ($isRangeSelection(selection)) {
+        if (
+          (selection as RangeSelection).anchor.offset ==
+          (selection as RangeSelection).focus.offset
+        ) {
+          setSelectStart((_old) => false);
+          setIsShowToolbar(null);
         }
-        console.log(selectStart);
-        console.log(selectEnd);
-        setActiveEditor(newEditor);
-        return true;
-      },
+        if (selectStart) {
+          const anchorNode: ElementNode = (
+            selection as RangeSelection
+          ).anchor.getNode();
+          const elementDOM = activeEditor.getElementByKey(anchorNode.getKey());
+          $setSelection(selection);
+          console.log("set selection success");
+          setIsShowToolbar(elementDOM);
+        }
+      }
+    });
+  }, [editor, activeEditor, selectStart]);
+
+  const handleSelectionChange = useCallback(
+    (_payload: void, _newEditor: LexicalEditor) => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        setSelectStart(
+          (selection as RangeSelection).anchor.offset !=
+            (selection as RangeSelection).focus.offset
+        );
+        editor.getRootElement()?.addEventListener("mouseup", handleMouseup);
+        console.log(
+          "change",
+          (selection as RangeSelection).anchor.offset,
+          (selection as RangeSelection).focus.offset,
+          selectStart
+        );
+      }
+      return false;
+    },
+    [editor, selectStart]
+  );
+
+  useEffect(() => {
+    const rootElement = editor.getRootElement();
+    // return mergeRegister(
+    const cmd = editor.registerCommand(
+      SELECTION_CHANGE_COMMAND,
+      handleSelectionChange,
       COMMAND_PRIORITY_CRITICAL
     );
-  }, [editor]);
+    //   editor.registerCommand(
+    //     CLICK_COMMAND,
+    //     (_payload, newEditor) => {
+    //       setSelectEnd(true);
+    //       $showToobar();
+    //       setActiveEditor(newEditor);
+    //       return false;
+    //     },
+    //     COMMAND_PRIORITY_CRITICAL
+    //   )
+    // );
+    return () => {
+      cmd(), rootElement?.removeEventListener("mouseup", handleMouseup);
+    };
+  }, [editor, activeEditor]);
 
   return <></>;
 }
